7510afd677234cebfda5766113fdeb03d86a14f6
Masa <katsukatsu.masa.0522@gmail.com>
2021-07-04 00:29:31 +0900

    ソートキーの設定と動作確認

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-var app = new Vue({
+new Vue({
     el: '#app',
     data: {
         message: 'Sort Clum In Table',
         users:[],
+        sort_key: "",
     },
     mounted() {
         axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(response => this.users = response.data)
-    }
+            .then(response => {
+                this.users = response.data
+                // console.log(response.data)
+                // console.log(this.users)
+            })
+    },
+    methods: {
+        sortBy(key) {
+            this.sort_key = key;
+        },
+    },
 })
