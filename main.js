cd59d2376d20e195f4e073e9b2060acc90c02733
Masa <katsukatsu.masa.0522@gmail.com>
2021-07-04 01:28:05 +0900

    JavaScriptのsortメソッドの確認

--- main.js
+++ main.js
@@ -19,3 +19,43 @@ new Vue({
         },
     },
 })
+
+let array = [5,3,8,9,1]
+array.sort(function(a,b){
+    if(a<b)return 1;
+    if(a>b)return -1;
+    return 0;
+});
+console.log(array.sort())
+
+// const items = [
+//     { name: 'Edward', number: 21 },
+//     { name: 'Sharpe', number: 37 },
+//     { name: 'And', number: 45 },
+//     { name: 'The', number: -12 },
+//     { name: 'Magnetic', number: 13 },
+//     { name: 'Zeros', number: 37 }
+// ];
+
+// items.sort(function (a, b) {
+//     return a.number - b.number
+// });
+// console.log(items.sort())
+
+const items = [
+    { name: 'Edward', number: 21 },
+    { name: 'Sharpe', number: 37 },
+    { name: 'And', number: 45 },
+    { name: 'The', number: -12 },
+    { name: 'Magnetic', number: 13 },
+    { name: 'Zeros', number: 37 }
+];
+
+items.sort(function(a, b) {
+    var nameA = a.name.toUpperCase();
+    var nameB = b.name.toUpperCase();
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+});
+console.log(items.sort())
